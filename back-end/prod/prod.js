d293a5e7fa5dd5891837e596abe5770176832065
ruslanNir <nirkuba19999@gmail.com>
2021-04-02 17:26:30 +0300

    xss middelwere

--- back-end/prod/prod.js
+++ back-end/prod/prod.js
@@ -1,6 +1,7 @@
 const helmet = require('helmet')
 const compression = require('compression')
 const cors = require('cors')
+const xss = require('xss-clean')
 
 // function allowCors(request, response, next) {
 //     response.header('Access-Control-Allow-Origin', '*');
@@ -16,10 +17,11 @@ const cors = require('cors')
 
 
 module.exports = function (app) {
-    app.use(helmet())
-    app.use(compression())
-    app.use(cors({
-        origin: '...',
-        methods: ['GET', 'POST']
-    }))
+   app.use(helmet())
+   app.use(compression())
+   app.use(xss())
+   app.use(cors({
+      origin: '...',
+      methods: ['GET', 'POST']
+   }))
 }
